38f6c8acbf48cf49bc7a32027a6bc630ce9db9c9
david <davidtnfsh@gmail.com>
2017-08-08 21:31:00 +0800

    把url pattern改回去

--- curso/urls.py
+++ curso/urls.py
@@ -2,6 +2,6 @@
 from django.conf.urls import url
 from curso import views
 urlpatterns = [
-	url(r'^get/search/$', views.search, name='search'),
-	url(r'^post/incWeight/$', views.incWeight, name='incWeight'),
+	url(r'^$', views.search, name='search'),
+	url(r'^incWeight/$', views.incWeight, name='incWeight'),
 ]
